1100eaf5ea970cd0eb5de04df776acba2d4edef8
shree <shree@DESKTOP-CLIOS5U>
2021-09-12 16:28:27 +0530

    update

--- src/basic/Addition.java
+++ src/basic/Addition.java
@@ -2,4 +2,14 @@ package basic;
 
 public class Addition {
 
+	
+	public static void main(String[] args) {
+		
+		int a= 20;
+		int b =10;
+		int c= a+b;
+		System.out.println(c);
+		
+		System.out.println(a);
+	}
 }
